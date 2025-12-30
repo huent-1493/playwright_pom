947ca33c4f9681aa6eeaa1f24a612883d8a31df2
nguyen_thi_hue-c+1-at-905418243710 <nguyen.thi.hue-c@sun-asterisk.com>
2025-12-30 15:31:47 +0700

    Task: Implement code gen

---
+++ tests/codegen.spec.js
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://demo.playwright.dev/todomvc/#/');
+  await page.getByRole('textbox', { name: 'What needs to be done?' }).click();
+  await page.getByRole('textbox', { name: 'What needs to be done?' }).fill('task A');
+  await page.getByRole('textbox', { name: 'What needs to be done?' }).press('Enter');
+  await page.getByRole('checkbox', { name: 'Toggle Todo' }).check();
+  await page.getByRole('button', { name: 'Delete' }).click();
+  await page.getByRole('textbox', { name: 'What needs to be done?' }).click();
+  await expect(page.getByRole('textbox', { name: 'What needs to be done?' })).toBeVisible();
+  await page.getByRole('heading', { name: 'todos' }).click();
+  await expect(page.getByRole('textbox', { name: 'What needs to be done?' })).toBeEmpty();
+});
